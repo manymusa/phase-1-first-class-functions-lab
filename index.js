11f0867ecc95d2c7f413cefb5f4b10d7b1505815
manymusa <adanalin113@gmail.com>
2022-08-03 19:43:09 -0400

    commit tree b4f501dcaeda1233524d56b63a9faf7aaeec2913

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) =>  [arr[0],arr[1]];
+
+const returnLastTwoDrivers = (arr) => [arr[arr.length-2],arr[arr.length-1]];
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier (num) {
+    return function (fare){
+         return num*fare;
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers (drivers,firstOrLast){
+    const whichDrivers = firstOrLast(drivers);
+    if(whichDrivers[0] ===  drivers[0]){
+        return returnFirstTwoDrivers(drivers);
+    } else {
+        return returnLastTwoDrivers(drivers);
+    }
+}
